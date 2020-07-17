62acac2ec4bb1223c117c0cb6f4c0949c816a092
ArnaudUNC <64565072+ArnaudUNC@users.noreply.github.com>
2020-07-17 11:08:56 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,13 +13,13 @@ posX = 2
 led.plot(posX, 0)
 let direction = 1
 basic.forever(function () {
+    basic.pause(1000)
     led.unplot(posX, 0)
     posX += direction
-    led.plot(posX, 0)
-    basic.pause(500)
     if (posX >= 4) {
         direction = -1
     } else if (posX <= 0) {
         direction = 1
     }
+    led.plot(posX, 0)
 })
